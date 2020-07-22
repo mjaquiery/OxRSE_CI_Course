6555899d3bee7018d72d01ca4083ad5aea3d3e11
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-07-22 15:05:28 +0100

    Update functionality.py
    Fixed overindentation.

--- ci_course/functionality.py
+++ ci_course/functionality.py
@@ -35,7 +35,7 @@ def minimum(*args):
         The minimum
     """
     if not any([isinstance(arg, numbers.Real) for arg in args]):
-            return
+        return
 
     the_min = float("inf")
     for arg in args:
